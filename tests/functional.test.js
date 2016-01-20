3bb8242abea28877126a0a5e9eec7c0fde0a2280
Fernando Jiménez Moreno <ferjmoreno@gmail.com>
2016-01-20 08:30:33 +0100

    More auth tests

--- tests/functional.test.js
+++ tests/functional.test.js
@@ -57,54 +57,107 @@ describe('Functional tests', function() {
   });
 
   /** Auth **/
+
   describe('Authentication', function() {
-    beforeEach(function() {
-      fxaMock.response();
+    describe('Auth token OK', function() {
+      beforeEach(function() {
+        fxaMock.response();
+      });
+
+      afterEach(function() {
+        fxaMock.reset();
+      });
+
+      Object.keys(routes).forEach(function(route) {
+        var methods = routes[route];
+        methods.forEach(function(method) {
+          if (method.auth) {
+            it('should reject ' + method.name.toUpperCase() + ' ' + route +
+               ' request without "Authorization" header', function(done) {
+              superagent[method.name](origin + route)
+                .end(function(e, res) {
+                  expect(e).to.be.an('object');
+                  expect(res.status).to.be.equal(401);
+                  expect(res.body.code).to.be.equal(401);
+                  expect(res.body.errno).to.be.equal(401);
+                  expect(res.body.error).to.be.equal('Unauthorized');
+                  expect(res.body.message).to.be.equal('Unauthorized');
+                  done();
+                });
+            });
+
+            it('should accept ' + method.name.toUpperCase() + ' ' + route +
+               ' request with "Authorization" header', function(done) {
+              superagent[method.name](origin + route)
+                .set('Authorization', 'Bearer token')
+                .set('Accept', 'application/json')
+                .end(function(e, res) {
+                  expect(res.status).to.not.be.equal(401);
+                  done();
+                });
+            });
+          } else {
+            it('should accept ' + method.name.toUpperCase() + ' ' + route +
+               ' request without "Authorization" header', function(done) {
+              superagent[method.name](origin + route)
+                .end(function(e, res) {
+                  expect(res.status).to.not.be.equal(401);
+                  done();
+                });
+            });
+          }
+        });
+      });
     });
 
-    afterEach(function() {
-      fxaMock.reset();
-    });
+    [{
+      test: 'Cannot verify auth token',
+      fxaMethod: 'verify'
+    }, {
+      test: 'Cannot fetch user profile',
+      fxaMethod: 'getProfile'
+    }].forEach(function(config) {
+      describe(config.test, function() {
+        beforeEach(function() {
+          var response = {};
+          response[config.fxaMethod] = {
+            status: 401,
+            body: {
+              code: 401,
+              errno: 401,
+              error: 'Unauthorized'
+            }
+          };
+          fxaMock.response(response);
+        });
 
-    Object.keys(routes).forEach(function(route) {
-      var methods = routes[route];
-      methods.forEach(function(method) {
-        if (method.auth) {
-          it('should reject ' + method.name.toUpperCase() + ' ' + route +
-             ' request without "Authorization" header', function(done) {
-            superagent[method.name](origin + route)
-              .end(function(e, res) {
-                expect(e).to.be.an('object');
-                expect(res.status).to.be.equal(401);
-                expect(res.body.code).to.be.equal(401);
-                expect(res.body.errno).to.be.equal(401);
-                expect(res.body.error).to.be.equal('Unauthorized');
-                expect(res.body.message).to.be.equal('Unauthorized');
-                done();
-              });
-          });
+        afterEach(function() {
+          fxaMock.reset();
+        });
 
-          it('should accept ' + method.name.toUpperCase() + ' ' + route +
-             ' request with "Authorization" header', function(done) {
-            superagent[method.name](origin + route)
-              .set('Authorization', 'Bearer token')
-              .set('Accept', 'application/json')
-              .end(function(e, res) {
-                expect(res.status).to.not.be.equal(401);
-                done();
-              });
+        Object.keys(routes).forEach(function(route) {
+          var methods = routes[route];
+          methods.forEach(function(method) {
+            if (!method.auth) {
+              return;
+            }
+            it('should reject ' + method.name.toUpperCase() + ' ' + route +
+               ' request with invalid "Authorization" header', function(done) {
+              superagent[method.name](origin + route)
+                .set('Authorization', 'Bearer token')
+                .set('Accept', 'application/json')
+                .end(function(e, res) {
+                  expect(e).to.be.an('object');
+                  expect(res.status).to.be.equal(401);
+                  expect(res.body.code).to.be.equal(401);
+                  expect(res.body.errno).to.be.equal(401);
+                  expect(res.body.error).to.be.equal('Unauthorized');
+                  expect(res.body.message).to.be.equal('Unauthorized');
+                  done();
+                });
+            });
           });
-
-        } else {
-          it('should accept ' + method.name.toUpperCase() + ' ' + route +
-             ' request without "Authorization" header', function(done) {
-            superagent[method.name](origin + route)
-              .end(function(e, res) {
-                expect(res.status).to.not.be.equal(401);
-                done();
-              });
-          });
-        }
+        });
       });
     });
   });
